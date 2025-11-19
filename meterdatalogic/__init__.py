fcacc8e4e4646b2e459435b498ba05a337f711a3
Tyler Harden <tyler-harden@outlook.com>
2025-11-19 10:43:11 +1000

    Create formats module
    Useful for compressing data to send to frontend web applications

--- meterdatalogic/__init__.py
+++ meterdatalogic/__init__.py
@@ -1,10 +1,21 @@
-from . import canon, types, utils, ingest, validate, transform, summary, pricing
+from . import (
+    canon,
+    types,
+    utils,
+    ingest,
+    validate,
+    formats,
+    transform,
+    summary,
+    pricing,
+)
 
 __all__ = [
     "canon",
     "types",
     "utils",
     "ingest",
+    "formats",
     "validate",
     "transform",
     "summary",
